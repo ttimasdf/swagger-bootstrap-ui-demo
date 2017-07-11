7824f876b46b9c39bf04d7351d4bf81079a2d965
肖玉明 <xiaoym@drore.com>
2017-07-11 11:55:25 +0800

    header参数支持

--- swagger-bootstrap-ui-demo/src/main/java/com/swagger/bootstrap/ui/demo/controller/TestController.java
+++ swagger-bootstrap-ui-demo/src/main/java/com/swagger/bootstrap/ui/demo/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.swagger.bootstrap.ui.demo.controller;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.swagger.bootstrap.ui.demo.common.RestMessage;
 import com.swagger.bootstrap.ui.demo.domain.ReqEntity;
 import io.swagger.annotations.Api;
@@ -7,6 +10,14 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 /***
  * @author <a href="mailto:xiaoymin@foxmail.com">xiaoymin@foxmail.com</a>
@@ -61,6 +72,80 @@ public class TestController {
     @ApiOperation(value = "ModelAttribute",notes = "ModelAttribute类型参数")
     public RestMessage reqbody1(@ModelAttribute ReqEntity reqEntity){
         return new RestMessage(reqEntity);
+    }
+
+    @PostMapping("/reqbody3")
+    @ApiOperation(value = "header参数",notes = "header参数")
+    @ApiImplicitParams({
+            @ApiImplicitParam(value = "code",name = "code",dataType = "string",paramType = "query"),
+            @ApiImplicitParam(value = "headerparam",name = "headerparam",dataType = "string",paramType = "header")
+    })
+    public RestMessage reqbody3(@RequestHeader(value = "headerparam") String headerparam,@RequestParam(value = "code") String code){
+        return new RestMessage(ImmutableMap.of("code",code,"header",headerparam));
+    }
+
+
+    @ApiOperation(value = "文件素材上传接口")
+    @ApiImplicitParam(name = "file",value = "文件流对象,接收数组格式",required = true,dataType = "MultipartFile")
+    @RequestMapping(value="/uploadMaterial",method = RequestMethod.POST)
+    @ResponseBody
+    public RestMessage uploadMaterial(@RequestParam(value="file") MultipartFile[] files, HttpServletRequest request) throws IOException {
+        //int mul=1*1024*1024;
+        String realPath=request.getSession().getServletContext().getRealPath("/upload");
+        File realFile=new File(realPath);
+        if (!realFile.exists()){
+            realFile.mkdirs();
+        }
+        List<Map> uploadFiles= Lists.newArrayList();
+        System.out.println("进入图片上传接口:"+files.length +"张");
+        for (MultipartFile file : files) {
+            File targetFile=new File(realFile,file.getOriginalFilename());
+            FileOutputStream fileOutputStream=null;
+            InputStream ins=null;
+            try{
+                fileOutputStream=new FileOutputStream(targetFile);
+                int i=-1;
+                byte[] bytes=new byte[1024*1024];
+                ins=file.getInputStream();
+                while ((i=ins.read(bytes))!=-1){
+                    fileOutputStream.write(bytes,0,i);
+                }
+            }catch (IOException e){
+            }finally {
+                closeQuilty(ins);
+                closeQuilty(fileOutputStream);
+            }
+            Map fileInfo= Maps.newHashMap();
+            fileInfo.put("id", UUID.randomUUID().toString());
+            fileInfo.put("url",targetFile.getPath());
+            fileInfo.put("original_name",targetFile.getName());
+            uploadFiles.add(fileInfo);
+        }
+        RestMessage rm=new RestMessage();
+        rm.setData(uploadFiles);
+        return rm;
+    }
+
+
+    private void closeQuilty(InputStream ins){
+        if (ins!=null){
+            try {
+                ins.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    private void closeQuilty(OutputStream out){
+        if (out!=null){
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 
