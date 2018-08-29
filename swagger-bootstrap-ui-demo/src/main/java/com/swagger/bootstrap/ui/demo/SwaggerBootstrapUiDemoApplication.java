c606eec100bc031a144019eba5013f556d75ff7d
肖玉明 <xiaoymin@foxmail.com>
2018-08-29 13:36:59 +0800

    springboot 1.5.15

--- swagger-bootstrap-ui-demo/src/main/java/com/swagger/bootstrap/ui/demo/SwaggerBootstrapUiDemoApplication.java
+++ swagger-bootstrap-ui-demo/src/main/java/com/swagger/bootstrap/ui/demo/SwaggerBootstrapUiDemoApplication.java
@@ -10,10 +10,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.*;
 import springfox.documentation.spring.web.SpringfoxWebMvcConfiguration;
 
 import java.net.InetAddress;
@@ -51,4 +55,93 @@ public class SwaggerBootstrapUiDemoApplication  implements WebMvcConfigurer{
 	}
 
 
+	@Override
+	public void configurePathMatch(PathMatchConfigurer pathMatchConfigurer) {
+
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer contentNegotiationConfigurer) {
+
+	}
+
+	@Override
+	public void configureAsyncSupport(AsyncSupportConfigurer asyncSupportConfigurer) {
+
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer defaultServletHandlerConfigurer) {
+
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry formatterRegistry) {
+
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
+
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry viewResolverRegistry) {
+
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> list) {
+
+	}
+
+	@Override
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> list) {
+
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> list) {
+
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> list) {
+
+	}
+
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> list) {
+
+	}
+
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> list) {
+
+	}
+
+	@Override
+	public Validator getValidator() {
+		return null;
+	}
+
+	@Override
+	public MessageCodesResolver getMessageCodesResolver() {
+		return null;
+	}
 }
