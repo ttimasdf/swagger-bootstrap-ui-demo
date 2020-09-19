1627acd0fe4530aa90a5c0bdf12f54391612d860
肖玉明 <xiaoymin@foxmail.com>
2020-09-19 11:48:10 +0800

    demo集成

---
+++ knife4j-springfox-boot-v3-demo/src/main/java/com/xiaominfo/knife4j/config/Knife4jConfig.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2018 Zhejiang xiaominfo Technology CO.,LTD.
+ * All rights reserved.
+ * Official Web Site: http://www.xiaominfo.com.
+ * Developer Web Site: http://open.xiaominfo.com.
+ */
+
+package com.xiaominfo.knife4j.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author <a href="mailto:xiaoymin@foxmail.com">xiaoymin@foxmail.com</a>
+ * 2020/09/18 11:04
+ * @since:knife4j-spring-boot2-demo 1.0
+ */
+@EnableOpenApi
+@Configuration
+public class Knife4jConfig {
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket=new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                //分组名称
+                .groupName("测试分组")
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.xiaominfo.knife4j.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("swagger-bootstrap-ui很棒~~~！！！")
+                .description("swagger-bootstrap-ui-demo RESTful APIs")
+                .termsOfServiceUrl("http://www.group.com/")
+                .contact(new Contact("八一菜刀","http://gitee.com/xiaoymin","xiaoymin@foxmail.com"))
+                .version("1.0")
+                .build();
+    }
+}
