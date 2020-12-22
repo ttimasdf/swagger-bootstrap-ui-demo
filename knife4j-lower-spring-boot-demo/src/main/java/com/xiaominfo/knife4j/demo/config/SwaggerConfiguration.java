d6dde904eb279a7c2d6257aa0dedee50a5595df6
肖玉明 <xiaoymin@foxmail.com>
2020-12-22 10:00:24 +0800

    低版本测试下载

--- knife4j-lower-spring-boot-demo/src/main/java/com/xiaominfo/knife4j/demo/config/SwaggerConfiguration.java
+++ knife4j-lower-spring-boot-demo/src/main/java/com/xiaominfo/knife4j/demo/config/SwaggerConfiguration.java
@@ -1,7 +1,6 @@
 
 package com.xiaominfo.knife4j.demo.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -15,7 +14,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@ConditionalOnProperty(value = {"swagger.enable"}, matchIfMissing = true)
 public class SwaggerConfiguration {
 
 
