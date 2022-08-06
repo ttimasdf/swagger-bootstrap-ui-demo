2429f7e884ac24c4dd240fcd2afe5b77633fbcab
xiaoyumin <xiaoymin@foxmail.com>
2022-08-06 18:36:59 +0800

    perf：部分请求示例-基于OAS3

--- knife4j-springdoc-openapi-demo/src/main/java/com/xiaominfo/knife4j/demo/web/BodyController.java
+++ knife4j-springdoc-openapi-demo/src/main/java/com/xiaominfo/knife4j/demo/web/BodyController.java
@@ -6,12 +6,12 @@ package com.xiaominfo.knife4j.demo.web;
 
 import com.xiaominfo.knife4j.demo.model.FileResp;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @since:knife4j-springdoc-openapi-demo
@@ -28,5 +28,39 @@ public class BodyController {
     public ResponseEntity<FileResp> body(@RequestBody FileResp fileResp){
         return ResponseEntity.ok(fileResp);
     }
+    @Operation(summary = "普通body请求-put")
+    @PutMapping("/bodyPut")
+    public ResponseEntity<FileResp> bodyPut(@RequestBody FileResp fileResp){
+        return ResponseEntity.ok(fileResp);
+    }
 
+    @Operation(summary = "普通body请求+Param")
+    @PostMapping("/bodyParam")
+    public ResponseEntity<FileResp> bodyParam(@RequestParam("name")String name,@RequestBody FileResp fileResp){
+        fileResp.setName(fileResp.getName()+",receiveName:"+name);
+        return ResponseEntity.ok(fileResp);
+    }
+
+    @Operation(summary = "普通body请求+Param+Header")
+    @Parameters({
+            @Parameter(name = "token",description = "请求token",required = true,in = ParameterIn.HEADER),
+            @Parameter(name = "name",description = "文件名称",required = true,in=ParameterIn.QUERY)
+    })
+    @PostMapping("/bodyParamHeader")
+    public ResponseEntity<FileResp> bodyParamHeader(@RequestHeader("token") String token, @RequestParam("name")String name,@RequestBody FileResp fileResp){
+        fileResp.setName(fileResp.getName()+",receiveName:"+name+",token:"+token);
+        return ResponseEntity.ok(fileResp);
+    }
+
+    @Operation(summary = "普通body请求+Param+Header+Path")
+    @Parameters({
+            @Parameter(name = "id",description = "文件id",in = ParameterIn.PATH),
+            @Parameter(name = "token",description = "请求token",required = true,in = ParameterIn.HEADER),
+            @Parameter(name = "name",description = "文件名称",required = true,in=ParameterIn.QUERY)
+    })
+    @PostMapping("/bodyParamHeaderPath/{id}")
+    public ResponseEntity<FileResp> bodyParamHeaderPath(@PathVariable("id") String id,@RequestHeader("token") String token, @RequestParam("name")String name,@RequestBody FileResp fileResp){
+        fileResp.setName(fileResp.getName()+",receiveName:"+name+",token:"+token+",pathID:"+id);
+        return ResponseEntity.ok(fileResp);
+    }
 }
