7b9d280462b339b66cabd1b8f05df734cb24cb90
xiaoyumin <xiaoymin@foxmail.com>
2022-08-07 12:21:11 +0800

    header参数bug

--- knife4j-springdoc-openapi-demo/src/main/java/com/xiaominfo/knife4j/demo/config/SwaggerConfig.java
+++ knife4j-springdoc-openapi-demo/src/main/java/com/xiaominfo/knife4j/demo/config/SwaggerConfig.java
@@ -10,6 +10,8 @@ package com.xiaominfo.knife4j.demo.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +32,9 @@ public class SwaggerConfig {
         String[] packagedToMatch = { "com.xiaominfo.knife4j.demo.web" };
         return GroupedOpenApi.builder().group("用户模块")
                 .pathsToMatch(paths)
+                .addOperationCustomizer((operation, handlerMethod) -> {
+                    return operation.addParametersItem(new HeaderParameter().name("groupCode").example("测试").description("集团code").schema(new StringSchema()._default("BR").name("groupCode").description("集团code")));
+                })
                 .packagesToScan(packagedToMatch).build();
     }
     @Bean
