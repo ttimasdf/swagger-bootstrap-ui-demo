bd4ad603d99869abc2b85192c8f6aab40534322c
xiaoyumin <xiaoymin@foxmail.com>
2022-12-04 22:23:40 +0800

    代码示例

--- knife4j-springdoc-openapi-demo/src/main/java/com/xiaominfo/knife4j/demo/Knife4jSpringdocOpenapiDemoApplication.java
+++ knife4j-springdoc-openapi-demo/src/main/java/com/xiaominfo/knife4j/demo/Knife4jSpringdocOpenapiDemoApplication.java
@@ -1,13 +1,36 @@
 package com.xiaominfo.knife4j.demo;
 
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
 
 @SpringBootApplication
 public class Knife4jSpringdocOpenapiDemoApplication {
-
+	private static Logger logger= LoggerFactory.getLogger(Knife4jSpringdocOpenapiDemoApplication.class);
+	@SneakyThrows
 	public static void main(String[] args) {
-		SpringApplication.run(Knife4jSpringdocOpenapiDemoApplication.class, args);
+		SpringApplication app=new SpringApplication(Knife4jSpringdocOpenapiDemoApplication.class);
+		ConfigurableApplicationContext application=app.run(args);
+		//ConfigurableApplicationContext application=SpringApplication.run(Knife4jSpringBootDemoApplication.class, args);
+		Environment env = application.getEnvironment();
+		logger.info("\n----------------------------------------------------------\n\t" +
+						"Application '{}' is running! Access URLs:\n\t" +
+						"Local: \t\thttp://localhost:{}\n\t" +
+						"External: \thttp://{}:{}\n\t"+
+						"Doc: \thttp://{}:{}/doc.html\n"+
+						"----------------------------------------------------------",
+				env.getProperty("spring.application.name"),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
 	}
 
 }
