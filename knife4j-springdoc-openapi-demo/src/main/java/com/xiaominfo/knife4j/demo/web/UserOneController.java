88e9488207b505ca6d7a9fa45b03308c9f824247
xiaoyumin <xiaoymin@foxmail.com>
2023-02-24 20:34:32 +0800

    test：测试代码

--- knife4j-springdoc-openapi-demo/src/main/java/com/xiaominfo/knife4j/demo/web/UserOneController.java
+++ knife4j-springdoc-openapi-demo/src/main/java/com/xiaominfo/knife4j/demo/web/UserOneController.java
@@ -7,11 +7,13 @@
 
 package com.xiaominfo.knife4j.demo.web;
 
+import com.xiaominfo.knife4j.demo.model.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +27,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user1")
 public class UserOneController {
+
+    @Operation(summary = "创建用户2",description = "根据姓名创建用户1")
+    @PostMapping("/createOne2")
+    public ResponseEntity<User> createOne1(@ParameterObject User user){
+        return ResponseEntity.ok(user);
+    }
+
+    @Operation(summary = "创建用户3",description = "根据姓名创建用户1")
+    @PostMapping("/createOne3")
+    public ResponseEntity<User> createOne2( User user){
+        return ResponseEntity.ok(user);
+    }
 
 
     @Operation(summary = "创建用户",description = "根据姓名创建用户")
