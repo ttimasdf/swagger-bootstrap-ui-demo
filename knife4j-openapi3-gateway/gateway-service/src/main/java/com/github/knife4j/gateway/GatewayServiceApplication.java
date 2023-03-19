f520b964f417e9245d01165d15afd8ca72968b0b
xiaoyumin <xiaoymin@foxmail.com>
2023-03-19 10:25:48 +0800

    test:测试gateway聚合

--- knife4j-openapi3-gateway/gateway-service/src/main/java/com/github/knife4j/gateway/GatewayServiceApplication.java
+++ knife4j-openapi3-gateway/gateway-service/src/main/java/com/github/knife4j/gateway/GatewayServiceApplication.java
@@ -1,13 +1,35 @@
 package com.github.knife4j.gateway;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+@Slf4j
 @SpringBootApplication
 public class GatewayServiceApplication {
 
+    @SneakyThrows
     public static void main(String[] args) {
-        SpringApplication.run(GatewayServiceApplication.class, args);
+        SpringApplication app=new SpringApplication(GatewayServiceApplication.class);
+        ConfigurableApplicationContext application=app.run(args);
+        //ConfigurableApplicationContext application=SpringApplication.run(Knife4jSpringBootDemoApplication.class, args);
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t"+
+                        "Doc: \thttp://{}:{}/doc.html\n"+
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
     }
 
 }
--- knife4j-openapi3-gateway/user-service/src/main/java/com/github/knife4j/user/UserServiceApplication.java
+++ knife4j-openapi3-gateway/user-service/src/main/java/com/github/knife4j/user/UserServiceApplication.java
@@ -1,13 +1,36 @@
 package com.github.knife4j.user;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+
+@Slf4j
 @SpringBootApplication
 public class UserServiceApplication {
 
+    @SneakyThrows
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication app=new SpringApplication(UserServiceApplication.class);
+        ConfigurableApplicationContext application=app.run(args);
+        //ConfigurableApplicationContext application=SpringApplication.run(Knife4jSpringBootDemoApplication.class, args);
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t"+
+                        "Doc: \thttp://{}:{}/doc.html\n"+
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
     }
 
 }
